0e556b4f3d672fb8c79eb0b6fc51c0d35decbbaa
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-25 09:32:45 +0100

    Update td1.js

--- js/TD1/td1.js
+++ js/TD1/td1.js
@@ -8,6 +8,13 @@ function max(a,b){
     return (a>b) ? a : b;
 }
 
+function power(n, x){
+    for (let i = 0; i < n; i++) {
+      a = a*n;
+    }
+    return a;
+}
+
 for (let i = 0; i < 100; i++) {
   console.log(i)
   if(i% 5 == 0){
@@ -20,3 +27,4 @@ for (let i = 0; i < 100; i++) {
 
 console.log("Min entre 224 et 42 : " + min(224,42));
 console.log("Max entre 0.1 et 0.009 : " + max(0.1,0.009));
+console.log("10 Puissance 7 : " + power(10,7));
