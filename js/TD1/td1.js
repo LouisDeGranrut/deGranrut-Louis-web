9b826c134aa80adb0d7c975a403a25f0ba8f65b7
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-25 09:10:09 +0100

    Update td1.js

--- js/TD1/td1.js
+++ js/TD1/td1.js
@@ -9,4 +9,4 @@ let max(a,b){
 }
 
 console.log("Min entre 224 et 42 : " + min(224,42);
-console.log("Max entre 0.1 et 0.009 : " + min(0.1,0.009);
+console.log("Max entre 0.1 et 0.009 : " + max(0.1,0.009);
