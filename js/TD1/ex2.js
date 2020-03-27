f0d86314c18eb0ed92b3023ebd92bac9653adb21
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-27 11:09:45 +0100

    Create ex2.js

---
+++ js/TD1/ex2.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function creerMultiplicateur(n) {
+  return (x) => {return x*n;};
+}
+
+
+
+console.log("Min entre 224 et 42 : " + min(224,42));
+console.log("Max entre 0.1 et 0.009 : " + max(0.1,0.009));
+console.log("10 Puissance 2 : " + power(10,2));
