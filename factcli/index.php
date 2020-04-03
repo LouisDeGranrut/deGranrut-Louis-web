034d3bfd94c7636d2ab436457317539a7b1a6296
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-04-03 19:04:26 +0200

    Update index.php

--- factcli/index.php
+++ factcli/index.php
@@ -6,7 +6,7 @@ use factcli\models\Client;
 use factcli\models\Facture;
 use factcli\outils\Outils;
 use factcli\conf\ConnectionFactory;
-
+//base de donnee
 ConnectionFactory::setConfig('conf.ini');
 ConnectionFactory::makeConnection();
 
@@ -22,24 +22,57 @@ $app->get('/client', function(){
     clientFact($id);
 })->name('repForm');
 
-//appel de outils pour afficher le formulaire de selection
+//appel de outils pour afficher le formulaire de selection du client
 function formulaire(){
   global $app;
-  $act = $app->urlFor('repform');
+  $act = $app->urlFor('repForm');
   Outils::headerHTML();
-  echo '<form action="" method="get" class="">';
-  echo '<select name="client[]">'."\n";
-  echo '<option value="ListeClient">--Liste des clients--</option>'."\n";
+
+  //echo '<form method='GET' action=*'$act'>';
+  //echo '<select name='client'>'."\n";
+  //echo '<option value='ListeClient'>--Clients--</option>'."\n";
+
+  echo "<form method='GET' action=''> <select name='client'>
+        <option value=''>--Liste des clients--</option>";
+
   $ListClient = Client::select( 'nomcli', 'id')->get();
   foreach ($ListClient as $row) {
-      $clientId=$row["id"];
-      $nomClient=$row["nomcli"];
-      echo "<option value='.$ListClient.'>".$nomClient."</option> \n";
+      //$clientId=$row["id"];
+      $nomClient=$row['nomcli'];
+      $id = $row->id;
+      echo "<option value='$id'>".$nomClient."</option> \n";
   }
-  echo '<input type="submit" name="tarif"  value="Valider">';
-  echo "</select>\n";
-  echo '</form>';
+        echo "<input type='submit' name='tarif'  value='Envoyer'>";
+      echo "</select>";
+    echo "</form>";
+
+  //si on clique sur envoyer
+  if (!isset($_POST['Envoyer'])){
+    $id = $_GET['client'];
+    $Facture = Facture::select('datefact', 'montant')->where('client_id','=',$id)->get();
+    echo "<h3>Liste des Factures de .$id.</h3>";
+    echo "</br>";
+     foreach ($Facture as $row) {
+         $date=$row['datefact'] ;
+         $prix=$row['montant'] ;
+         echo $date ."\n" . $prix . " €" ."<br>";
+      }
+   }
 }
+
+/*function test(){
+  if (!isset($_POST['Envoyer'])){
+    $id = $_GET['client'];
+    $Facture = Facture::select('datefact', 'montant')->where('client_id','=',$id)->get();
+    echo "<h3>Liste des Factures:</h3>";
+    echo "</br>";
+     foreach ($Facture as $row) {
+         $date=$row["datefact"] ;
+         $prix=$row["montant"] ;
+         echo $date ."\n" . $prix . " €" ."<br>";
+      }
+   }
+}*/
 
 $app->run();
 Outils :: footerHTML();
